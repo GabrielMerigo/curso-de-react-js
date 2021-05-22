c657a4394b321ad8df8b35c9498e6a0804cb0ae9
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-05-22 15:12:34 -0300

    Avançando com react.js

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+const Equipe = (props) => {
+  return (
+    <div>
+      <Sobre name={props.name} funcao={props.funcao} idade={props.idade} />
+      <Midia
+        twitter={props.twitter}
+        fb={props.facebook}
+        instagram={props.instagram}
+      />
+      <hr />
+    </div>
+  )
+}
+
+const Sobre = (props) =>
+  <p>Esse é o {props.name} ele é o {props.funcao} da equipe com {props.idade} anos.</p>
+
+const Midia = (props) => {
+  return (
+    <div>
+      <h3>Midias Sociais:</h3>
+      <p>
+        <strong>Facebook:</strong> <a href={props.fb}>GAY</a> <br />
+        <strong>Twitter:</strong> {props.twitter} <br />
+        <strong>Instagram:</strong> {props.instagram}
+      </p>
+    </div>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Conheça nosso equipe:</h1>
+      <Equipe name="Gabriel"
+        funcao="Programador"
+        idade="25"
+        twitter="@merigod"
+        facebook="https://google.com"
+        instagram="@gabriel_merigo"
+      />
+      <Equipe name="Jorge"
+        funcao="Designer UX/UI"
+        idade="17"
+        twitter="@Luquicas_XDXD"
+        facebook="https://google.com"
+        instagram="@Lucas__prado"
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
