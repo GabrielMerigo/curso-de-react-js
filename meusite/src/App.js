679787275216f6aa05591aec34f0639506321c6e
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-05-23 14:18:45 -0300

    Update App.js

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,56 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Equipe = (props) => {
+class Equipe extends Component {
+  render(){
+    return (
+      <div>
+        <h2>Olá {this.props.name} </h2>
+      </div>
+    )
+  }
+}
+
+function App () {
   return (
     <div>
-      <Sobre name={props.name} funcao={props.funcao} idade={props.idade} />
-      <Midia
-        twitter={props.twitter}
-        fb={props.facebook}
-        instagram={props.instagram}
-      />
-      <hr />
+      <h2>Conheça a nossa equipe</h2>
+      <Equipe name="Gabriel"/>
     </div>
   )
 }
-
-const Sobre = (props) =>
-  <p>Esse é o {props.name} ele é o {props.funcao} da equipe com {props.idade} anos.</p>
-
-const Midia = (props) => {
-  return (
-    <div>
-      <h3>Midias Sociais:</h3>
-      <p>
-        <strong>Facebook:</strong> <a href={props.fb}>GAY</a> <br />
-        <strong>Twitter:</strong> {props.twitter} <br />
-        <strong>Instagram:</strong> {props.instagram}
-      </p>
-    </div>
-  )
-}
-
-
-function App() {
-  return (
-    <div>
-      <h1>Conheça nosso equipe:</h1>
-      <Equipe name="Gabriel"
-        funcao="Programador"
-        idade="25"
-        twitter="@merigod"
-        facebook="https://google.com"
-        instagram="@gabriel_merigo"
-      />
-      <Equipe name="Jorge"
-        funcao="Designer UX/UI"
-        idade="17"
-        twitter="@Luquicas_XDXD"
-        facebook="https://google.com"
-        instagram="@Lucas__prado"
-      />
-    </div>
-  )
-}
-
 export default App
