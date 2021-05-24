81bd5fd06d8bfc2f9b4d51594dc5cb7b62ecd1fc
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-05-24 01:22:37 -0300

    Update App.js

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
 
-class Equipe extends Component {
+class App extends Component{
+
+  constructor(props){
+    super(props)
+    this.state = {
+      hora: '00:00:00'
+    };
+  }
+
+  componentDidMount(){
+    setInterval(() => {
+      this.setState({hora: new Date().toLocaleTimeString()})
+    }, 1000)
+  }
+
+  componentDidUpdate(){
+    console.log('Atualizou!!');
+  }
+
   render(){
-    return (
+    return(
       <div>
-        <h2>Olá {this.props.name} </h2>
-        <h2>Seu cargo é {this.props.cargo}</h2>
+        <h2>{this.state.hora}</h2>
       </div>
     )
   }
 }
 
-function App () {
-  return (
-    <div>
-      <h2>Conheça a nossa equipe</h2>
-      <Equipe name="Gabriel" cargo="Programador" idade="18"/>
-    </div>
-  )
-}
-export default App
+export default App;
