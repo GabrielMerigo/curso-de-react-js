e625dbfe10f727ad9db384ac5b7021de60c019b2
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-05-24 22:25:19 -0300

    Eventos no React

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,28 +1,11 @@
 import React, { Component } from 'react';
+import Membro from './component/Membro' 
 
 class App extends Component{
-
-  constructor(props){
-    super(props)
-    this.state = {
-      hora: '00:00:00'
-    };
-  }
-
-  componentDidMount(){
-    setInterval(() => {
-      this.setState({hora: new Date().toLocaleTimeString()})
-    }, 1000)
-  }
-
-  componentDidUpdate(){
-    console.log('Atualizou!!');
-  }
-
   render(){
     return(
       <div>
-        <h2>{this.state.hora}</h2>
+        <Membro nome="Visitante"/>
       </div>
     )
   }
