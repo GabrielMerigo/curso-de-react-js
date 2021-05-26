b0107269d410d6c504a80f0ec4e3c3b3e9ec4e1a
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-05-25 23:42:50 -0300

    Formulário

--- meusite/src/App.jsx
+++ meusite/src/App.jsx
@@ -1,30 +1,13 @@
 import React, { Component } from 'react';
-import Feed from './component/Feed';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      feed: [
-        {id: 1, username: 'Matheus', curtidas: 10, comentarios: 4},
-        {id: 2, username: 'Lucas', curtidas: 152, comentarios: 24},
-        {id: 4, username: 'Amanda', curtidas: 140, comentarios: 15},
-        {id: 4, username: 'Amanda', curtidas: 1, comentarios: 1}
-      ]
-    }
-  }
-
   render() {
     return (
       <div>
-        {
-          this.state.feed.map((user) => {
-            return(
-              <Feed key={user.id} username={user.username} 
-              curtidas={user.curtidas} comentarios={user.comentarios} />
-            )
-          })
-        }
+        <h1>Formulário</h1>
+        <form>
+          <input type="text" value={this.state.nome}/>
+        </form>
       </div>
     )
   }
