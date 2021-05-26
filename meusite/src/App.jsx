b8540b9ca19a27ffd599cc59df8a554b1d082296
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-05-26 00:04:39 -0300

    Formulário feito

--- meusite/src/App.jsx
+++ meusite/src/App.jsx
@@ -1,12 +1,53 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      nome: '',
+      email: '',
+      senha: ''
+    }
+
+    this.changeNome = this.changeNome.bind(this)
+    this.changeEmail = this.changeEmail.bind(this)
+    this.cadastrar = this.cadastrar.bind(this)
+  }
+
+  changeNome(e){
+    const valorNomeDigitado = e.target.value
+    this.setState({nome: valorNomeDigitado})
+  }
+
+  changeEmail(e){
+    const valorEmailDigitado = e.target.value;
+    this.setState({email: valorEmailDigitado})
+  }
+
+  cadastrar(e){
+    if(!Boolean(this.state.nome) || !Boolean(this.state.email) || !Boolean(this.state.email)){
+      alert('por favor, preencha todos os dados.')
+      e.preventDefault()
+      return false
+    }
+
+    alert(
+      `Dados inseridos no banco de dados -
+       Nome: ${this.state.nome}
+       E-mail: ${this.state.email}
+       Senha: ${this.state.senha}`
+    )
+  }
+
   render() {
     return (
       <div>
         <h1>Formulário</h1>
-        <form>
-          <input type="text" value={this.state.nome}/>
+        <form onSubmit={this.cadastrar}>
+          Nome: <input type="text" value={this.state.nome} onChange={this.changeNome}/> <br/>
+          E-mail: <input type="email" value={this.state.email} onChange={this.changeEmail}/><br/>
+          Senha: <input type="text" value={this.state.senha} onChange={(e) => this.setState({senha: e.target.value })}/><br/>
+          <button type="submit">Enviar</button>
         </form>
       </div>
     )
