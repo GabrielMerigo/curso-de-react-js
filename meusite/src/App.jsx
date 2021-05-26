381cc28f6ae9bfd277ab3c20fa54bcd960ac7440
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-05-26 01:06:16 -0300

    teste

--- meusite/src/App.jsx
+++ meusite/src/App.jsx
@@ -1,37 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-class App extends Component {
+
+class App extends Component{
   constructor(props){
     super(props)
-    this.state = {
-      form: {
-        nome: '',
-        email: '',
-        senha: ''
-      }
-    }
 
-    this.dadosForm = this.dadosForm.bind(this)
+    this.state = {};
   }
 
-  dadosForm(e){
-    const { form } = this.state
-    form[e.target.name] = e.target.value 
-    this.setState({form: form})
-  }
-
-  render() {
-    return (
+  render(){
+    return(
       <div>
-        <h1>Formulário</h1>
-        <form>
-          Nome: <input name="nome" type="text" value={this.state.form.nome} onChange={this.dadosForm}/><br/>
-          E-mail: <input name="email" type="email" value={this.state.form.email} onChange={this.dadosForm} /><br/>
-          Senha: <input name="senha" type="text" value={this.state.form.senha} onChange={this.dadosForm}/><br/>
-          <button type="submit">Enviar</button>
-        </form>
-
-        <h3>{this.state.form.nome}</h3>
+        <img src={require('./biscoito.png')}/>
       </div>
     )
   }
