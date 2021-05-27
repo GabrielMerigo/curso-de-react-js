d41f56bdea3ed94c2f4a63584c2b4b5479840b5d
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-05-26 23:26:04 -0300

    Projeto Cronometro

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,48 +1,14 @@
-import React, { Component } from 'react'
-import Image from './assets/biscoito.png'
+import React, { Component } from 'react';
 import './style.css'
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      texto: ''
-    };
-    
-    this.frases = [
-      'A vida me ensinou que chorar alivia, mas sorrir torna tudo mais bonito',
-      'Nunca troque o que você mais quer na vida pelo que você mais quer no mome.',
-      'É preciso coragem para ser diferente e muita competência para fazer a diferença.'
-    ]
-
-    this.quebraBiscoito = this.quebraBiscoito.bind(this)
-  }
-
-  quebraBiscoito(){
-    const randomNumber = Math.round(Math.random() * 2)
-    const frases = this.frases 
-    this.setState({texto: `"${frases[randomNumber]}"`})
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <img src={Image} alt="Favicon" className="img"/>
-        <h3 className="textoFrase">{this.state.texto}</h3>
-        <Botao acao={this.quebraBiscoito}/>
-      </div>
-    )
-  }
-}
-
-class Botao extends Component{
+class App extends Component{
   render(){
     return(
       <div>
-        <button onClick={this.props.acao} className="btn">Abrir Biscoito</button>
+        13
       </div>
     )
   }
 }
 
-export default App;
+export default App
